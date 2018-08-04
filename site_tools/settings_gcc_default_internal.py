ab0df7ba79daeae59aa837c6aaa418c6eee667bb
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2018-08-04 04:05:51 +0200

    [gcc] Fix using gcc on macOS

--- site_tools/settings_gcc_default_internal.py
+++ site_tools/settings_gcc_default_internal.py
@@ -53,6 +53,9 @@ def generate(env, **kw):
         env['AR'] = prefix + 'gcc-ar' + suffix
         env['NM'] = prefix + 'gcc-nm' + suffix
         env['RANLIB'] = prefix + 'gcc-ranlib' + suffix
+        if sys.platform == "darwin":
+            env['AR'] = prefix + 'ar'
+            env['RANLIB'] = prefix + 'ranlib'
 
     env['OBJCOPY'] = prefix + 'objcopy'
     env['OBJDUMP'] = prefix + 'objdump'
